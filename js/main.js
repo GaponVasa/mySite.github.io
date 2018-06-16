e93904f66b8bf6ae55210f3d0e8cb239efc2c9cf
GaponVasa <gapon.vasa@ukr.net>
2018-06-16 19:37:48 +0300

    change logical main.js with localStorage

--- js/main.js
+++ js/main.js
@@ -1,60 +1,91 @@
-$(function(){
-	(function(){
-		var lastClickDiv;
+"use strict";
+(function(){
+	var lastClickDiv;
 
-		function anim(){
-			var heightWel = $('#welcome').height();
-			if(heightWel > 0){
-				$('#welcome').animate({height: '0px', opacity: 0}, 500);
-			};
+	function anim(){
+		var heightWel = $('#welcome').height();
+		if(heightWel > 0){
+			$('#welcome').animate({height: '0px', opacity: 0}, 500);
 		};
+	};
 
-		function animIdDiv(idDiv){
-			if(lastClickDiv != idDiv){
-				if(idDiv == 'works'){
-					localStorage.setItem('works', 1);
-					localStorage.setItem('lastClickDiv', lastClickDiv);
-				};
-				$('.' + lastClickDiv).removeClass('active');
-				$('#' + lastClickDiv).animate({height: 'hide', opacity: 'hide'}, 500);
-				$('.' + idDiv).addClass('active');
-				$('#' + idDiv).delay(450).animate({height: 'show', opacity: 'show'}, 500);
-			};
+	function animIdDiv(idDiv){
+		$('.' + lastClickDiv).removeClass('active');
+		$('#' + lastClickDiv).animate({height: 'hide', opacity: 'hide'}, 500);
+		$('.' + idDiv).addClass('active');
+		$('#' + idDiv).delay(450).animate({height: 'show', opacity: 'show'}, 500);
+	};
+
+	function getSetLocalStorage(name, str){
+		if(str){
+			localStorage.setItem(name, str);
+		}else{
+			return localStorage.getItem(name);
 		};
+	};
 
-		$('.nav').on('click', function(event){
-			var target = event.target;
-			var targClass = $(target).attr('class').split(' ');
-			switch(targClass[0]){
-				case 'about': anim();
-					animIdDiv(targClass);
-				break;
-				case 'works': anim();
-					animIdDiv(targClass);
-				break;
-				case 'contact': anim();
-					animIdDiv(targClass);
-				break;
-			};
-			lastClickDiv = targClass;
-		});
+	function changeLanguageButton(){
+		if($('#myonoffswitch:checked').length == 1){
+	    	$('.en').animate({opacity:'hide'}, 300).css('display','none');
+	    	$('.uk').animate({opacity:'show'}, 300).css('display','block');
+	    	if(lastClickDiv){lastClickDiv[1] = 'uk'};
+	    }else{
+	    	$('.uk').animate({opacity:'hide'}, 300).css('display','none');
+	    	$('.en').animate({opacity:'show'}, 300).css('display','block');
+	    	if(lastClickDiv){lastClickDiv[1] = 'en'};
+	    };
+	}
 
-		if(localStorage.getItem('works') == '1'){
-			lastClickDiv = localStorage.getItem('lastClickDiv');
-			anim();
-			animIdDiv('works');
-			lastClickDiv = 'works';
-			localStorage.setItem('works', 0);
+	function changeLanguage(language){
+		if(language === 'uk'){
+			$('.en').css('display','none');
+	    	$('.uk').css('display','block');
+		}else{
+			$('.uk').css('display','none');
+	    	$('.en').css('display','block');
+		}
+	}
+
+	$('.nav').on('click', function(event){
+		var target = event.target;
+		var targClass = $(target).attr('class').split(' ');
+		var classToString = targClass[0] + ' ' + targClass[1];
+		switch(targClass[0]){
+			case 'about': anim();
+				animIdDiv(targClass);
+				getSetLocalStorage('works', '0');
+			break;
+			case 'works': anim();
+				animIdDiv(targClass);
+				getSetLocalStorage('works', '1');
+				getSetLocalStorage('lastClick', classToString);
+			break;
+			case 'contact': anim();
+				animIdDiv(targClass);
+				getSetLocalStorage('works', '0');
+			break;
 		};
+		lastClickDiv = targClass;
+	});
 
-		$('.onoffswitch-label').on('click', function(){
-		    if($('#myonoffswitch:checked').length == 1){
-		    	$('.en').animate({opacity:'hide'}, 300).css('display','none');
-		    	$('.uk').animate({opacity:'show'}, 300).css('display','block');
-		    }else{
-		    	$('.uk').animate({opacity:'hide'}, 300).css('display','none');
-		    	$('.en').animate({opacity:'show'}, 300).css('display','block');
+	$('.onoffswitch-label').on('click', changeLanguageButton);
+
+    window.onkeydown = function(event){
+    	console.log(event)
+    	if(event.key === 'F5'){
+    		window.onbeforeunload = function() {
+    			localStorage.clear();
 		    };
-		});
-	}());
-});
+    	}
+    };
+
+	if(getSetLocalStorage('works') === '1'){
+		var stringTpClass = getSetLocalStorage('lastClick');
+		lastClickDiv = stringTpClass.split(' ');
+		changeLanguage(lastClickDiv[1]);
+		anim();
+		animIdDiv('works');
+	};
+
+	
+}());
